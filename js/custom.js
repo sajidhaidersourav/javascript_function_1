2fb0be0bb3e03962aa9a352146a47aa8c36a8dd1
Sajid Haider <165992305+sajidhaidersourav@users.noreply.github.com>
2024-09-29 13:05:00 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,191 @@
+// text change
+function textChange(){
+
+    document.getElementById('tc').innerHTML=("MASHALLAH");
+
+}
+// show time
+function showTime(){
+
+    document.getElementById('st').innerHTML=Date();
+
+}
+// color change
+function colorChange(){
+
+    document.getElementById('clr').style.color="red";
+
+}
+// font size change
+function fontSize(){
+
+    document.getElementById('fnt_siz').style.fontSize="30px";
+
+}
+// font family
+function fontFamily(){
+
+    document.getElementById('ff').style.fontFamily="cursive";
+
+}
+
+// display show hide
+function displayShow(){
+
+    document.getElementById('boxshow').style.display="block";
+
+}
+function displayHide(){
+
+    document.getElementById('boxshow').style.display="none";
+
+}
+// bordre redus
+function raidus(){
+
+    document.getElementById('raidus').style.borderRadius="20px";
+    
+}
+// circle
+function circle(){
+
+    document.getElementById('circle').style.borderRadius="50%";
+
+}
+// border
+function border(){
+
+    document.getElementById('border').style.border="5px solid black";
+
+}
+// undrline
+function underLine(){
+
+    document.getElementById('undr').style.textDecoration="underline";
+
+}
+// mark
+function mark(){
+
+    document.getElementById('mark').style.color="orangered";
+
+}
+// upper lower case
+function uppcase(){
+
+    document.getElementById('upp_lw').style.textTransform="uppercase";
+
+}
+function lowcase(){
+
+    document.getElementById('upp_lw').style.textTransform="lowercase";
+
+}
+// radius top-left bottom-right
+function radius_tl_br(){
+
+    document.getElementById('tl_br').style.borderRadius="25% 10%";
+
+}
+// radius top-left bottom-right
+function radius_tb_bl(){
+
+    document.getElementById('tr_bl').style.borderRadius="10% 25%";
+
+}
+// squre
+function squreBox(){
+
+    document.getElementById('sqr_box').style.borderRadius="0%";
+
+}
+// alart 
+function alartBox(){
+
+    alert('PLEASE SUBSCRIBE  ME :-) ');
+
+}
+// blub in/off
+function blubOn(){
+
+    document.getElementById('blub').src="img/pic_bulbon.gif";
+
+}
+function blubOff(){
+
+    document.getElementById('blub').src="img/pic_bulboff.gif";
+
+}
+// cold / hot coffe 
+function coldCoffe(){
+
+    document.getElementById('coffe').src="img/coll,coffe.png";
+
+}
+function hotCoffe(){
+
+    document.getElementById('coffe').src="img/hot_coffe.gif";
+
+}
+// happy sad 19
+function hp_sd(){
+
+    document.getElementById('happy').src="img/happy.png";
+
+}
+function sd_hp(){
+
+    document.getElementById('happy').src="img/sad.png";
+
+}
+// dashed
+function line_throug(){
+
+    document.getElementById('line_throug').style.textDecoration="line-through ";
+
+}
+// over line
+function overline(){
+    document.getElementById('overline').style.textDecoration="overline";
+}
+// lettter spacing
+function lettterSpacing(){
+    document.getElementById('lettterSpacing').style.letterSpacing="10px";
+}
+// word specing
+function wordSpecing(){
+    document.getElementById('wordSpacing').style.wordSpacing="20px";
+}
+// text align strat
+function textAlingStart(){
+    document.getElementById('textAlignStrat').style.textAlign="left";
+}
+// text align end
+function textAlignEnd(){
+    document.getElementById('textAlignEnd').style.textAlign="right";
+}
+// text aling center
+function textAlignCenter(){
+    document.getElementById('textAlignCenter').style.textAlign="center";
+}
+// font weight
+function fontWeight(){
+    document.getElementById('font_weight').style.fontWeight="700";
+}
+
+// border doted
+function bdrTop(){
+    document.getElementById('bdrTop').style.borderTop= "3px dashed red";
+}
+// border doted
+function dotid(){
+    document.getElementById('brdrDoted').style.borderLeft= "3px dashed red";
+}
+// border solid
+function solid(){
+    document.getElementById('solid').style.borderRight= "3px solid red";
+}
+// border bottom
+function bottom(){
+    document.getElementById('bottom').style.borderBottom= "3px solid red";
+}
